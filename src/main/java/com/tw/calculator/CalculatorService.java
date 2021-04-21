e19076f6a35398ddbcdf43f9d4cfafac3b34e5b5
saireddyavs <avulurivenkatasaireddy@gmail.com>
2021-04-21 15:18:04 +0530

    [Pavan/Sai Reddy] Add. Logic for logs

--- src/main/java/com/tw/calculator/CalculatorService.java
+++ src/main/java/com/tw/calculator/CalculatorService.java
@@ -1,20 +1,26 @@
 package com.tw.calculator;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class CalculatorService {
 
+    Logger logger = LoggerFactory.getLogger(CalculatorService.class);
+
     @RequestMapping(value = "/add", method = RequestMethod.POST)
     @ResponseStatus(HttpStatus.OK)
     public double getAdditionResult(@RequestBody  Params params) {
+        logger.info("Addition method invoked");
         return new Calculator().add(params.getValueOne(), params.getValueTwo());
     }
 
     @RequestMapping(value = "/multiply", method = RequestMethod.POST)
     @ResponseStatus(HttpStatus.OK)
     public double getMultiplicationResult(@RequestBody  Params params) {
+        logger.info("Subtract Method Invoked");
         return new Calculator().multiply(params.getValueOne(), params.getValueTwo());
     }
 
