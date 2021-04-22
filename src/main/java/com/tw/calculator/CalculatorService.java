343cbd84d98886482dfc2e4279adb80db1dc13c5
saireddyavs <avulurivenkatasaireddy@gmail.com>
2021-04-22 08:10:41 +0530

    [Pavan/Sai Reddy] Add. Logic for enable/Disable endpoint at runtime

--- src/main/java/com/tw/calculator/CalculatorService.java
+++ src/main/java/com/tw/calculator/CalculatorService.java
@@ -1,5 +1,6 @@
 package com.tw.calculator;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -8,28 +9,41 @@ import org.springframework.web.bind.annotation.*;
 public class CalculatorService {
     public static boolean divisionViewFlag = false;
 
+    @Autowired
+    private CalculatorConfiguration calculatorConfiguration;
+
     @RequestMapping(value = "/add", method = RequestMethod.POST)
     @ResponseStatus(HttpStatus.OK)
-    public double getAdditionResult(@RequestBody  Params params) {
+    public double getAdditionResult(@RequestBody Params params) {
         return new Calculator().add(params.getValueOne(), params.getValueTwo());
     }
 
     @RequestMapping(value = "/multiply", method = RequestMethod.POST)
     @ResponseStatus(HttpStatus.OK)
-    public double getMultiplicationResult(@RequestBody  Params params) {
+    public double getMultiplicationResult(@RequestBody Params params) {
         return new Calculator().multiply(params.getValueOne(), params.getValueTwo());
     }
 
     @RequestMapping(value = "/divide", method = RequestMethod.POST)
-    public ResponseEntity<Double> getDivisionResult(@RequestBody  Params params){
+    public ResponseEntity<Double> getDivisionResult(@RequestBody Params params) {
         final double divide = new Calculator().divide(params.getValueOne(), params.getValueTwo());
-        if (divisionViewFlag)return new ResponseEntity<Double>(divide, HttpStatus.OK);
+        if (divisionViewFlag) return new ResponseEntity<Double>(divide, HttpStatus.OK);
         else return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
     }
 
-    @RequestMapping(value = "/toggleDivision", method = RequestMethod.POST)
+
+    @RequestMapping(value = "/enablefeature", method = RequestMethod.POST)
     @ResponseStatus(HttpStatus.OK)
-    public void setDivisionViewFlag(@RequestBody  boolean status){
-        divisionViewFlag = status;
+    public String enablefeature() {
+        calculatorConfiguration.setMultiply(true);
+        return "Multiply Is enabled";
     }
- }
+
+    @RequestMapping(value = "/disablefeature", method = RequestMethod.POST)
+    @ResponseStatus(HttpStatus.OK)
+    public String disablefeature() {
+        calculatorConfiguration.setMultiply(false);
+        return "Multiply is Disabled";
+    }
+
+}
---
+++ src/main/java/com/tw/calculator/ServiceFilter.java
@@ -0,0 +1,44 @@
+package com.tw.calculator;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+@Component
+public class ServiceFilter implements Filter {
+    @Autowired
+    private CalculatorConfiguration calculatorConfiguration;
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+
+        try {
+            Method method = calculatorConfiguration.getClass().getMethod("is" + StringUtils.capitalize(req.getRequestURI().substring(1)));
+            boolean result = (boolean) method.invoke(calculatorConfiguration);
+            if (result) {
+                chain.doFilter(request, response);
+            } else {
+
+                ((HttpServletResponse) res).sendError(HttpServletResponse.SC_NOT_FOUND, "The Endpoint Is Not Found");
+            }
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
