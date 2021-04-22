d3b37256771e64df6dce33d478eddcc37c0384d6
saireddyavs <avulurivenkatasaireddy@gmail.com>
2021-04-22 08:12:38 +0530

    [Pavan/Sai Reddy] Refactor. Name change

--- src/main/java/com/tw/calculator/CalculatorController.java
+++ src/main/java/com/tw/calculator/CalculatorController.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-public class CalculatorService {
+public class CalculatorController {
     public static boolean divisionViewFlag = false;
 
     @Autowired
